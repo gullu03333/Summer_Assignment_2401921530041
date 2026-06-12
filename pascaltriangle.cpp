b376afe700964f5aa006e48a431785ed9c8be8a5
gullu03333 <ankitgamail.2003@gmail.com>
2026-06-12 20:30:23 +0530

    Create pascaltriangle.cpp

---
+++ pascaltriangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v(numRows);
+        int i,j;
+        for(i=0;i<numRows;i++){
+            v[i].resize(i+1);
+            for(j=0;j<=i;j++){
+             if(j==0||j==i) v[i][j]=1;
+             else
+            v[i][j]=v[i-1][j]+v[i-1][j-1];    
+
+            }
+        }        
+        return v;
+    }
+};
